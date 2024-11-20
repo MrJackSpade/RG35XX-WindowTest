3430e7bca1ac4029001ddd731d4bdf2d914e3e42
MrJackSpade <loffredo.ap@gmail.com>
2024-11-20 11:39:41 -0800

    no message

---
+++ ButtonTestPage.cs
@@ -0,0 +1,34 @@
+﻿using RG35XX.Core.GamePads;
+using RG35XX.Libraries.Controls;
+
+namespace WindowTest
+{
+    internal class ButtonTestPage : Page
+    {
+        private readonly string _buttonText;
+
+        public ButtonTestPage(string buttonText)
+        {
+            _buttonText = buttonText;
+
+            Label label = new()
+            {
+                Text = _buttonText,
+                FontSize = 1f,
+                Bounds = new(0, 0, .5f, .1f)
+            };
+
+            this.AddControl(label);
+        }
+
+        public override void OnKey(GamepadKey key)
+        {
+            if (key == GamepadKey.B_DOWN)
+            {
+                this.Close();
+            }
+
+            base.OnKey(key);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using RG35XX.Core.Drawing;
+using RG35XX.Libraries;
+using RG35XX.Libraries.Controls;
+
+namespace WindowTest
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Application application = new(640, 480);
+
+            Window window = new()
+            {
+                Title = "Window Test",
+                Bounds = new(0, 0, 1, 1)
+            };
+
+            IconView iconView = new();
+
+            Bitmap iconImage = new("Images/IExplore.png");
+
+            for (int i = 0; i < 42; i++)
+            {
+                Icon b = new()
+                {
+                    Text = $"Button {i}",
+                    FontSize = 0.5f,
+                    Image = iconImage,
+                };
+
+                iconView.AddControl(b);
+            }
+
+            Page page = new();
+
+            page.AddControl(window);
+
+            window.AddControl(iconView);
+
+            application.OpenPage(page);
+
+            await application.Execute();
+        }
+    }
+}
